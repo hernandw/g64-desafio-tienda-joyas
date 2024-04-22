791149cefb1ae8aa9a34ed1be67dfcd1294e152b
Williams Hernandez <hernandw@gmail.com>
2024-04-22 15:52:08 -0400

    :tada: funciones de hateoas y category

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { hateoas, joyasCategory } from "../controller/controller.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.send("Hello World!");
 })
+
+router.get("/joyas", hateoas)
+
+router.get("/joyas/categoria/:categoria", joyasCategory)
 
 export default router
